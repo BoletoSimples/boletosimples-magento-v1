347274adcde2e61e28d9ac077db23d9e4727f996
Dhyego Fernando <dhyegofernando@gmail.com>
2017-03-07 11:05:20 -0400

    Add expiry days and cpf cnpj attribute to system config

---
+++ src/app/code/community/Codigo5/BoletoSimples/Model/System/Config/Source/Customer/Attributes.php
@@ -0,0 +1,48 @@
+<?php
+
+class Codigo5_BoletoSimples_Model_System_Config_Source_Customer_Attributes
+{
+    protected $_options;
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if (is_null($this->_options)) {
+            $this->_options = array();
+            $_helper = Mage::helper('codigo5_boletosimples');
+            $attributes = Mage::getModel('customer/entity_attribute_collection')
+                ->addVisibleFilter();
+
+            foreach ($attributes as $attribute) {
+                $this->_options[] = array(
+                    'label' => $attribute->getFrontendLabel(),
+                    'value' => $attribute->getAttributeCode()
+                );
+            }
+
+            $this->_options = $_helper->addBlankOption($this->_options);
+        }
+
+        return $this->_options;
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $array = array();
+
+        foreach ($this->toOptionArray() as $option) {
+            $array[$option['value']] = $option['label'];
+        }
+
+        return $array;
+    }
+}
